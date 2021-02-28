77a8a774717e7f0a0353bcbd0b9e05ff7c7958db
dustinfht <dustin.codes02@gmail.com>
2021-02-28 23:21:08 +0100

    refactored project-structure

--- contact_book/configuraton.py
+++ contact_book/configuraton.py
@@ -8,5 +8,5 @@ class Configuration:
 
     def load(self):
         config_parser = ConfigParser()
-        config_parser.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
+        config_parser.read(os.path.join(os.path.dirname(__file__), '../config.cfg'))
         self.message_enter_last_name = utils.parse_string(config_parser.get("messages", "enter_last_name"))
